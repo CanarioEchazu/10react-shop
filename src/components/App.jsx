c9a6f06e14973d82bd8a5651c38c873a48c26d71
Dario Canario Echazu <105083756+CanarioEchazu@users.noreply.github.com>
2022-12-10 19:47:10 -0300

    terminado el ejemplo de React
    # hay un error al activar SASS
    la linea a poner es la siguiente
    
    ## npm i --save-dev node-sass sass-loader -D

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/global.scss';
+
+const App = () => {
+    return(
+        <h1> Canario con React, vamos Argentina Campeon</h1>
+    );
+}
+
+export default App;
